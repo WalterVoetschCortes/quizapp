780f303df3a3a6f555efd49b0aec4729b914d9e4
WalterVoetschCortes <walter.vxd@googlemail.com>
2020-10-31 09:36:32 +0100

    developed API.ts and App.tsx

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,16 @@
 /**
  * logic for fetching data from the API
 */
+
+export enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard"
+}
+
+export const fetschQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `http://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const data = await (await fetch(endpoint)).json();
+    console.log(data);
+
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+// Types:
+import {Difficulty, fetschQuizQuestions} from "./API";
 // Components:
-import QuestionCard from "./components/QuestionCard";
 
+const TOTAL_QUESTIONS = 10;
 
 //main component in the application
 const App = () => {
+
+    //loading state:
+    const [loading, setLoading] = useState(false);
+    const [questions, setQuestions] = useState([]);
+    const [number, setNumber] = useState(0);
+    const [userAnswer, setUserAnswers] = useState([]);
+    const [score, setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(true);
+
+    console.log(fetschQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+
 
     // function to start the quiz:
     const startTrivia = async () => {
@@ -28,7 +40,14 @@ const App = () => {
             </button>
             <p className="score">Score:</p>
             <p>Loading Questions...</p>
-            <QuestionCard />
+            {/*<QuestionCard
+                questionNr = {number + 1}
+                totalQuestions = {TOTAL_QUESTIONS}
+                question={questions[number].question}
+                answers={questions[number].answers}
+                userAnswer={userAnswer ? userAnswer[number] : undefined}
+                callback={checkAnswer}
+            />*/}
             <button className="next" onClick={nextQuestion}>
                 Next Question
             </button>
